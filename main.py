a1364f8f08b13fba72f5eaea83a559c1f7fce230
long5619 <tranbaolong2025@gmail.com>
2025-04-15 00:29:22 +1000

    update

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import os
 import requests
-import time
 
-DISCORD_TOKEN = os.getenv("")
-WEBHOOK_URL = os.getenv("")
-def send_alert(msg):
-    payload = {"content": msg}
-    requests.post(WEBHOOK_URL, json=payload)
-
-def main_loop():
-    while True:
-        # Giả lập tín hiệu SMC mẫu
-        signal = "[SMC Alert] EUR/USD vừa xuất hiện CHoCH + OB Buy zone. Winrate 82% 🔵"
-        send_alert(signal)
-        time.sleep(3600)  # gửi mỗi 1 tiếng
+def send_alert():
+    webhook_url = os.getenv("webhook_url")
+    message = {
+        "content": "**[SMC Alert]** EUR/USD vừa xuất hiện CHoCH + OB Buy zone. Winrate 82%. 📈",
+    }
+    requests.post(webhook_url, json=message)
 
 if __name__ == "__main__":
-    main_loop()
+    send_alert()
